da22e289a5c9c8156f9b1cd73c3075651cb4e623
sanketbakshi1981 <sanket.bakshi@gmail.com>
2025-12-29 04:28:47 +0000

    Replace title text with ArtLoop logo text image

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -59,10 +59,10 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'ArtLoop',
+      title: '',
       logo: {
         alt: 'ArtLoop Logo',
-        src: 'img/ArtLoop-Logo-Small-Transparent.png',
+        src: 'img/ArtLoop-Logo-Text-Transparent.png',
       },
       items: [
         {to: '/', label: 'Upcoming Event', position: 'left'},
