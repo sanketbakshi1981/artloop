7202f049fdfbb1bd054376b519979aa309b85c15
sanketbakshi1981 <sanket.bakshi@gmail.com>
2026-01-08 13:30:50 +0000

    updated prod url

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -21,7 +21,8 @@ const config: Config = {
 
   // Set the production url of your site here
   // TODO: Change to 'https://artloop.us' once DNS is configured
-  url: 'https://gray-water-0a5b86410.1.azurestaticapps.net',
+  // url: 'https://gray-water-0a5b86410.1.azurestaticapps.net',
+  url: 'https://artloop.us',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
